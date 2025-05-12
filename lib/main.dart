7bc4c1049b9442fd018eba9f4087cdf5f9079b59
Kiyan Salehniya <kiyan.salehniya@sabanciuniv.edu>
2025-05-12 16:16:25 +0300

    map changes

--- lib/main.dart
+++ lib/main.dart
@@ -17,21 +17,42 @@ import 'package:cs_projesi/pages/SettingsPage.dart';
 import 'package:cs_projesi/pages/sign_in_page.dart';
 import 'package:cs_projesi/pages/sign_up_page.dart';
 import 'package:cs_projesi/pages/mapPage.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
-  runApp(MaterialApp(
-    title: 'NatureSync',
-    debugShowCheckedModeBanner: false,
-    initialRoute: '/',
-    routes: {
-      '/': (context) => const SignInPage(),
-      '/SignUp': (context) => const SignUpPage(),
-      '/ProfileCreation': (context) => ProfileCreationPage(),
-      '/HomePage': (context) => HomePage(),
-      '/EventPage': (context) => EventPage(event: ModalRoute.of(context)!.settings.arguments as Event),
-      '/SettingsPage': (context) => const SettingsPage(),
-      '/MapPage': (context) => const MapPage(),
-      '/QuestionMarkPage': (context) => const QuestionMarkPage(),
-    },
-  ));
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'NatureSync',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: 'RobotoSerif', // This applies globally
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const SignInPage(),
+        '/SignUp': (context) => const SignUpPage(),
+        '/ProfileCreation': (context) => ProfileCreationPage(),
+        '/HomePage': (context) => HomePage(),
+        '/EventPage': (context) =>
+            EventPage(event: ModalRoute.of(context)!.settings.arguments as Event),
+        '/SettingsPage': (context) => const SettingsPage(),
+        '/MapPage': (context) => const MapPage(),
+        '/QuestionMarkPage': (context) => const QuestionMarkPage(),
+      },
+    );
+  }
 }
