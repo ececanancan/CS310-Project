b0bb6ee8906f45cee00aeab1355d91555d247f1e
merthasanaslan <merthasanaslan325@gmail.com>
2025-05-18 14:54:53 +0300

    Some changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:cs_projesi/models/profile.dart';
 import 'package:cs_projesi/models/event.dart';
-import 'package:cs_projesi/data/profile_data.dart';
-import 'package:cs_projesi/data/event_data.dart';
 import 'package:cs_projesi/widgets/eventCardWidget.dart';
 import 'package:cs_projesi/widgets/storyBarWidget.dart';
 import 'package:cs_projesi/widgets/navigationBarWidget.dart';
@@ -11,7 +9,6 @@ import 'package:cs_projesi/pages/eventPage.dart';
 import 'package:cs_projesi/pages/profileCreation.dart';
 import 'package:cs_projesi/pages/questionMarkPage.dart';
 import 'package:cs_projesi/pages/ProfilePage.dart';
-import 'package:cs_projesi/data/UserProfile_data.dart';
 import 'package:cs_projesi/models/UserProfile.dart';
 import 'package:cs_projesi/pages/SettingsPage.dart';
 import 'package:cs_projesi/pages/sign_in_page.dart';
